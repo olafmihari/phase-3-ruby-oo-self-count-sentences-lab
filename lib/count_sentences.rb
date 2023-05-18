d1db3c3b764eca36b4192de23376022c673d61bf
Olaf Mihari <olafnjenga@gmail.com>
2023-05-18 16:34:00 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,19 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    end_with?('.', '?', '!')
   end
 
   def question?
-
+    end_with?('?')
   end
 
   def exclamation?
-
+    end_with?('!')
   end
 
   def count_sentences
-
+    scan(/[.?!]+/).size
   end
 end
